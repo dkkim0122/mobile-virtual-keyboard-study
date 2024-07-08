929e3283484b50909f32541fb36fabb1362ce739
kim-dokyung <dokyung.kim@flitto.com>
2024-07-08 10:05:26 +0900

    innerHeight와 visualHeight 표시 요소 제작

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// Scripts for scroll-behavior-study
+let innerHeight;
+let visualViewportHeight;
+
+const innerHeightEl = document.getElementById("inner-height");
+const visualHeightEl = document.getElementById("visual-height");
+
+function updateHeightValue() {
+  innerHeightEl.innerText = `innerHeight: ${window.innerHeight}`;
+  visualHeightEl.innerText = `visualHeight: ${window.visualViewport.height}`;
+}
+
+updateHeightValue();
+
+window.visualViewport.addEventListener("resize", (e) => {
+  updateHeightValue();
+});
