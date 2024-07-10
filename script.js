2130886183329ca5110cfc8c2cd6fab55589d180
kim-dokyung <dokyung.kim@flitto.com>
2024-07-10 20:05:10 +0900

    resize 시 content 높이와 footer bottom 변경

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let innerHeight;
 let visualViewportHeight;
 
+const headerEl = document.getElementById("header");
+const contentEl = document.getElementById("content");
+const footerEl = document.getElementById("footer");
+
 const innerHeightEl = document.getElementById("inner-height");
 const visualHeightEl = document.getElementById("visual-height");
 
@@ -9,8 +13,20 @@ function updateHeightValue() {
   visualHeightEl.innerText = `visualHeight: ${window.visualViewport.height}`;
 }
 
-updateHeightValue();
-
-window.visualViewport.addEventListener("resize", (e) => {
+function onResize() {
   updateHeightValue();
-});
+
+  if (!headerEl || !contentEl || !footerEl) return;
+
+  const visualViewportHeight = window.visualViewport.height;
+  const headerHeight = headerEl.clientHeight;
+  const footerHeight = footerEl.clientHeight;
+
+  const newContentHeight = visualViewportHeight - headerHeight - footerHeight;
+  const newFooterBottom = window.innerHeight - visualViewportHeight;
+  contentEl.style.height = `${newContentHeight}px`;
+  footerEl.style.bottom = `${newFooterBottom}px`;
+}
+
+updateHeightValue();
+window.visualViewport.addEventListener("resize", onResize);
